647436e18fff0a680b8d8d42857c3a2a2d6074d3
Daniel Berg <Berg.iodaniel@gmail.com>
2022-06-30 16:45:45 +0200

    basic db setup

---
+++ server/Data/Models/Highscore.cs
@@ -0,0 +1,9 @@
+namespace server.Data.Models
+{
+  public class Highscore
+  {
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public int Score { get; set; }
+  }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<PuliContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("PuliContext")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -12,8 +16,17 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+  var services = scope.ServiceProvider;
+
+  var context = services.GetRequiredService<PuliContext>();
+  context.Database.EnsureCreated();
+  // DbInitializer.Initialize(context);
 }
 
 app.UseHttpsRedirection();
