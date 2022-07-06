d5696c18f1c327bcac86ebe6b13e08ef239ef381
Daniel Berg <Berg.iodaniel@gmail.com>
2022-07-06 14:43:13 +0200

    lobby styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen">
       <Header></Header>
       <Main></Main>
       <Footer></Footer>
--- client/src/components/Lobby/Lobby.tsx
+++ client/src/components/Lobby/Lobby.tsx
@@ -6,27 +6,56 @@ const Lobby = (props: ILobbyProps) => {
   const [room, setRoom] = useState<string>("");
 
   return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          props.joinRoom(user, room);
-        }}
-      >
-        <input
-          type="text"
-          placeholder="name"
-          onChange={(e) => setUser(e.target.value)}
-        ></input>
-        <input
-          type="text"
-          placeholder="room"
-          onChange={(e) => setRoom(e.target.value)}
-        ></input>
-        <button type="submit" disabled={!user || !room}>
-          Join
-        </button>
-      </form>
+    <div className="hero bg-base-200 full-height">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Join a room!</h1>
+          <p className="py-6">Enter a username and room to join.</p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              props.joinRoom(user, room);
+            }}
+            className="card-body"
+          >
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Username</span>
+              </label>
+              <input
+                required
+                type="text"
+                placeholder="username"
+                className="input input-bordered"
+                onChange={(e) => setUser(e.target.value)}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Room</span>
+              </label>
+              <input
+                required
+                type="text"
+                placeholder="room"
+                className="input input-bordered"
+                onChange={(e) => setRoom(e.target.value)}
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button
+                className="btn btn-primary mb-4"
+                type="submit"
+                disabled={!user || !room}
+              >
+                Join
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
